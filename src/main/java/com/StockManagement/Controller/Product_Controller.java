1357a4f781afee80a6e9c805ec1ec5cffafdb7c2
Anderson Zango <azango94@gmail.com>
2024-01-03 16:21:21 +0200

    Finishing Product CRUD

--- src/main/java/com/StockManagement/Controller/Product_Controller.java
+++ src/main/java/com/StockManagement/Controller/Product_Controller.java
@@ -6,6 +6,8 @@ import com.StockManagement.Domain.Product;
 import com.StockManagement.Repository.Product_Repository;
 import com.StockManagement.Services.Product_Services;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,18 +19,38 @@ public class Product_Controller {
     private Product_Repository productRepository;
     @Autowired
     private Product_Services productServices;
-    @GetMapping
-    public List<Product_ResponseDTO> getProducts(){
-        List<Product_ResponseDTO> productList = productRepository.findAll().stream().
-                map(Product_ResponseDTO::new).toList();
-        return productList;
-    }
 
     @PostMapping
-    public Product_RequestDTO insertProduct(@RequestBody  Product_RequestDTO data){
+    public Product_RequestDTO insertProduct(@RequestBody Product_RequestDTO data) {
         Product product = new Product(data);
         productServices.setProductAvailabilty(product);
         productRepository.save(product);
         return data;
     }
+
+    @GetMapping
+    public List<Product_ResponseDTO> getProducts() {
+        List<Product_ResponseDTO> productList = productRepository.findAll().stream().
+                map(Product_ResponseDTO::new).toList();
+        return productList;
+    }
+
+    @PutMapping
+    public Product_RequestDTO replaceProduct(@RequestBody Product_RequestDTO data) {
+        Product product = productServices.findProductByNameORThrowException(data.name());
+        product.setPrice(data.price());
+        product.setStock_quantity(data.stock_quantity());
+        productServices.setProductAvailabilty(product);
+        productRepository.save(product);
+        return data;
+    }
+
+    @DeleteMapping
+
+    public Product_RequestDTO deleteProduct(@RequestBody Product_RequestDTO data) {
+        Product product = productServices.findProductByNameORThrowException(data.name());
+        productRepository.delete(product);
+        return data;
+    }
+
 }
--- src/main/java/com/StockManagement/Services/Product_Services.java
+++ src/main/java/com/StockManagement/Services/Product_Services.java
@@ -4,18 +4,26 @@ import com.StockManagement.Domain.Availability;
 import com.StockManagement.Domain.Product;
 import com.StockManagement.Repository.Product_Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 
 public class Product_Services {
     @Autowired
     private Product_Repository productRepository;
+    public Product findProductByNameORThrowException(String name){
+        return productRepository.findProductByName(name).
+                orElseThrow(()->new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product Not Found"));
+    }
+
     public void setProductAvailabilty(Product product) {
-        if(product.getStock_quantity()!= 0){
+        if (product.getStock_quantity() != 0) {
             product.setAvailability(Availability.in_stock);
-        }else {
-        product.setAvailability(Availability.out_of_stock);}
+        } else {
+            product.setAvailability(Availability.out_of_stock);
+        }
 
     }
 }
